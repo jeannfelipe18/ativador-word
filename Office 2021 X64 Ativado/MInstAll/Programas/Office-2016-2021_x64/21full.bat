402ea33fa743a68f231ceb9f0eecfe755ea38ef0
jeannfelipe18 <135741976+jeannfelipe18@users.noreply.github.com>
2025-01-04 15:43:38 -0300

    Delete Office 2021 X64 Ativado directory

--- Office 2021 X64 Ativado/MInstAll/Programas/Office-2016-2021_x64/21full.bat
+++
@@ -1,6 +0,0 @@
-Office\helper.exe /S /INSTALL /PROX64 /WORD /EXCEL /POWERPOINT /OUTLOOK /ACCESS /ONENOTE /PUBLISHER /LANG=PT-BR /KMS
-if exist  "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" "%userprofile%\desktop"
-if exist  "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PowerPoint.lnk" copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PowerPoint.lnk" "%userprofile%\desktop"
-if exist  "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk" copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk" "%userprofile%\desktop"
-if exist  "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" "%userprofile%\desktop"
-
